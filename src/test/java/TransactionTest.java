e21ec860a2261f25018bcd4b006ab8b874a90661
Harry <harryjp1995@gmail.com>
2018-10-08 21:05:28 +0100

    test: Adds basic tests for Transaction

--- src/test/java/TransactionTest.java
+++ src/test/java/TransactionTest.java
@@ -6,11 +6,35 @@ import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertEquals;
 
+import java.text.SimpleDateFormat;
+import java.text.Format;
+import java.util.Date;
+import java.util.ArrayList;
+import java.util.Hashtable;
+
 public class TransactionTest {
   private Transaction transaction;
 
   @Before
   public void setup() {
-    transaction = new Transaction();
+    Date date = new Date();
+    transaction = new Transaction(date, 10.95, 10.95);
+  }
+
+  @Test
+  public void hasTheRightDateAndFormat() {
+    Format dateFormatter = new SimpleDateFormat("dd/MM/yyyy");
+    String strDate = dateFormatter.format(date);
+    assertEquals(strdate, transaction.date);
+  }
+
+  @Test
+  public void hasTheRightAmountAndFormat() {
+    assertEquals("10.95", transaction.amount);
+  }
+
+  @Test
+  public void hasTheRightBalanceAndFormat() {
+    asertEquals("10.95", transaction.balance);
   }
 }
