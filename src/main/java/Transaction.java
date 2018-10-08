daee86973f3a4b546a1d82dfd5eca39757d5c0bb
Harry <harryjp1995@gmail.com>
2018-10-08 21:39:28 +0100

    feat: Transaction now adds all relevant info to an instance variable

--- src/main/java/Transaction.java
+++ src/main/java/Transaction.java
@@ -9,11 +9,19 @@ class Transaction {
   public String date;
   public String amount;
   public String balance;
+  public Hashtable<String, String> info = new Hashtable<>();
 
   Transaction(Date inputDate, double inputAmount, double inputBalance) {
     Format dateFormatter = new SimpleDateFormat("dd/MM/yyyy");
     date = dateFormatter.format(inputDate);
     amount = String.valueOf(inputAmount);
     balance = String.valueOf(inputBalance);
+    addInfo();
+  }
+
+  private void addInfo() {
+    info.put("date", date);
+    info.put("amount", amount);
+    info.put("balance", balance);
   }
 }
