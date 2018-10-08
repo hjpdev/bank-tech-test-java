933c444502b31877c7fe56275278145643426c45
Harry <harryjp1995@gmail.com>
2018-10-08 14:46:07 +0100

    test: Adds first failing test for AccountStatement

--- src/test/java/AccountStatementTest.java
+++ src/test/java/AccountStatementTest.java
@@ -9,6 +9,10 @@ import com.sun.source.tree.AssertTree;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertEquals;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Hashtable;
+
 public class AcountStatementTest {
 
   private AccountStatement accountStatement;
@@ -16,5 +20,19 @@ public class AcountStatementTest {
   @Before
   public void setup() {
     accountStatement = new AccountStatement();
+    SimpleDateFormat dateFormatter = new SimpleDateFormat("dd/MM/yyyy");
+    SimpleDateFormat timeFormatter = new SimpleDateFormat("HH:mm:ss");
+    Date date = new Date();
+  }
+
+  @Test
+  public void addTransactionAddsDetails() {
+    Date formattedDate = dateFormatter.format(date);
+    Hashtable testLog = new Hashtable<>();
+    testLog.put("date", formattedDate);
+    testLog.put("amount", 10);
+    testLog.put("balance", 10);
+    accountStatement.addTransaction(formattedDate, 10, 10);
+    assertArrayEquals(accountStatement.log == testLog);
   }
 }
