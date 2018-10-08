f303d820e6e1648633f60f2f9180301a1b1d2810
Harry <harryjp1995@gmail.com>
2018-10-08 16:06:05 +0100

    feat: Account Statement passes test for adding transaction to log

--- src/main/java/AccountStatement.java
+++ src/main/java/AccountStatement.java
@@ -1,3 +1,31 @@
+import java.text.SimpleDateFormat;
+import java.text.Format;
+import java.util.Date;
+import java.util.ArrayList;
+import java.util.Hashtable;
+
 class AccountStatement {
+
+  private ArrayList<Hashtable<String, String>> log = new ArrayList<Hashtable<String, String>>();
+
+  AccountStatement() {
+    ArrayList log[];
+  }
+
+  public ArrayList returnLog() {
+    return log;
+  }
+
+  public void addTransaction(Date date, double amount, double balance) {
+    Format dateFormatter = new SimpleDateFormat("dd/MM/yyyy");
+    String strDate = dateFormatter.format(date);
+    String strAmount = String.valueOf(amount);
+    String strBalance = String.valueOf(balance);
+    Hashtable<String, String> transaction = new Hashtable<>();
+    transaction.put("date", strDate);
+    transaction.put("amount", strAmount);
+    transaction.put("balance", strBalance);
+    log.add(transaction);
+  }
 
 }
--- src/test/java/AccountStatementTest.java
+++ src/test/java/AccountStatementTest.java
@@ -8,33 +8,35 @@ import com.sun.source.tree.AssertTree;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 import java.text.SimpleDateFormat;
+import java.text.Format;
 import java.util.Date;
 import java.util.ArrayList;
 import java.util.Hashtable;
 
-public class AcountStatementTest {
+public class AccountStatementTest {
 
   private AccountStatement accountStatement;
 
   @Before
   public void setup() {
     accountStatement = new AccountStatement();
-    SimpleDateFormat dateFormatter = new SimpleDateFormat("dd/MM/yyyy");
-    Date date = new Date();
   }
 
   @Test
   public void addTransactionAddsDetails() {
+    Date date = new Date();
+    Format dateFormatter = new SimpleDateFormat("dd/MM/yyyy");
     String formattedDate = dateFormatter.format(date);
     ArrayList<Hashtable> testLog = new ArrayList<Hashtable>();
     Hashtable<String, String> testTrans = new Hashtable<>();
     testTrans.put("date", formattedDate);
-    testTrans.put("amount", "10.00");
-    testTrans.put("balance", "10.00");
+    testTrans.put("amount", "10.0");
+    testTrans.put("balance", "10.0");
     testLog.add(testTrans);
-    accountStatement.addTransaction(formattedDate, 10.00, 10.00);
-    assertArrayEquals(accountStatement.log == testLog);
+    accountStatement.addTransaction(date, 10.00, 10.00);
+    assertEquals(testLog, accountStatement.returnLog());
   }
 }
