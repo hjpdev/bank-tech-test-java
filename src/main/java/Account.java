f07cd96d47925c46898ae23c78935b358ca3cfbe
Harry <harryjp1995@gmail.com>
2018-10-11 12:29:01 +0100

    task: Adds second constructor function to allow for overriding defaults; Adds overdraft

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -5,15 +5,27 @@ import java.util.Date;
 public class Account {
 
   private double balance;
+  private double overdraft;
   private AccountStatement statement = new AccountStatement();
 
   public Account() {
     double balance = 0.00;
+    double overdraft = 0.00;
     AccountStatement statement = new AccountStatement();
+  }
+
+  public Account(double balance, double overdraft, AccountStatement statement) {
+    this.balance = balance;
+    this.overdraft = overdraft;
+    this.statement = statement;
   }
 
   public double currentBalance() {
     return balance;
+  }
+
+  public double overdraftLimit() {
+    return overdraft;
   }
 
   public AccountStatement returnStatement() {
